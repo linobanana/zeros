32cdb7692cc30006066ec0d25761596b66f842f3
Anhelina Lakhtanava <linobanana@gmail.com>
2019-10-01 21:12:01 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let array = expression.split('*');
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].indexOf('!!') !== -1) {
+      let number = Number(array[i].slice(0, array[i].indexOf('!')));
+      if (number % 2 === 0) {
+        let counter = 0;
+        for (let j = 2; j <= number; j += 2) {
+          if (j === 2) {
+            counter = BigInt(j);
+          } else {
+              counter *= BigInt(j);
+            }
+        }
+        array[i] = counter.toString();
+      } else {
+          let counter = 0;
+          for (let j = 1; j <= number; j += 2) {
+            if (j === 1) {
+              counter = BigInt(1);
+            } else {
+                counter *= BigInt(j);
+            }
+          }
+          array[i] = counter.toString();
+      }
+    } else {
+      let number = Number(array[i].slice(0, array[i].indexOf('!')));
+      let counter = 0;
+      for (let j = 1; j <= number; j++) {
+        if (j === 1) {
+          counter = BigInt(1);
+        } else {
+          counter *= BigInt(j);
+        }
+      }
+      array[i] = counter.toString();
+    }
+  }
+  let arrayMultiplicationResult = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (i === 0) {
+      arrayMultiplicationResult = BigInt(array[i]);
+    } else {
+      arrayMultiplicationResult *= BigInt(array[i]);
+    }
+  }
+  arrayMultiplicationResult = arrayMultiplicationResult.toString();
+  let reverseString = arrayMultiplicationResult.split('').reverse().join('');
+  let numberOfZeros = 0;
+  for (let i = 0; i < reverseString.length; i++) {
+    if (reverseString[i] === '0') {
+      numberOfZeros++;
+    }
+    if (reverseString[i + 1] !== '0') {
+      break;
+    }
+  }
+  return numberOfZeros;
 }
